d91c33e0be71b812e963acd2b89a24bfa2884371
Nahuel <coronel98nahuel@gmail.com>
2022-05-14 13:49:57 -0300

    version1.0

---
+++ src/components/nav/navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar =()=>{
+    return(
+        <nav>
+            <img src="https://www.wsj.com/coupons/static/shop/32174/logo/Nike-coupons.png" alt=""></img>
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Productos</a></li>
+                <li><a href="#">Categorias</a></li>
+                <li><a href="#">Servicio</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
