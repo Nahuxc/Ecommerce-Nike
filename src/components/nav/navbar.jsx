b30c727064c67ee509e6c12943111f327f432653
Nahuel <coronel98nahuel@gmail.com>
2022-05-15 16:32:28 -0300

    version1.0

--- src/components/nav/navbar.jsx
+++ src/components/nav/navbar.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import "./navbar.css"
+
 
 const Navbar =()=>{
     return(
-        <nav>
-            <img src="https://www.wsj.com/coupons/static/shop/32174/logo/Nike-coupons.png" alt=""></img>
-            <ul>
-                <li><a href="#">Inicio</a></li>
-                <li><a href="#">Productos</a></li>
-                <li><a href="#">Categorias</a></li>
-                <li><a href="#">Servicio</a></li>
+        <nav className="nav">
+            <img className="logo" src="https://www.wsj.com/coupons/static/shop/32174/logo/Nike-coupons.png" alt=""></img>
+            <ul className="nav-menu">
+                <li className="nav-menu_li"><a href="#">Inicio</a></li>
+                <li className="nav-menu_li"><a href="#">Productos</a></li>
+                <li className="nav-menu_li"><a href="#">Categorias</a></li>
+                <li className="nav-menu_li"><a href="#">Servicio</a></li>
             </ul>
+            <button className="btn-interaz" ><i class="fa-solid fa-cart-shopping"></i></button>
+            <button  className="btn-interaz"><i class="fa-solid fa-user"></i></button>
         </nav>
     );
 }
