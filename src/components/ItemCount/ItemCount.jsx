7dd2452f4064e45b7f3c6049f48f09fa8340589f
Nahuel <coronel98nahuel@gmail.com>
2022-05-22 20:02:03 -0300

    version1.0

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,15 +1,34 @@
 import "./ItemCount.css"
+import { useState } from "react";
 
-const ItemCount = ()=>{
+
+const ItemCount = (props)=>{
+
+    const [count, setCount] = useState(parseInt(props.initial));
+
+    const onAdd = () => {
+		// condicional para evitar estar por encima del máximo (stock disponible)
+		if (count < props.stock) {
+			setCount(count + 1);
+		}
+	};
+
+	const onSubtraction = () => {
+		// condicional para evitar estar por debajo del valor inicial otorgado
+		if (count > props.initial) {
+			setCount(count - 1);
+		}
+	};
+
     return(
         <div className="box-btnprod">
-            <button className="btn-rest">
+            <button className="btn-rest" onClick={onSubtraction}>
                 -
             </button>
                 <span className="span-num-prod">
-                    0
+                    {count}
                 </span>
-            <button className="btn-sum">
+            <button className="btn-sum" onClick={onAdd}>
                 +
             </button>
         </div>
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -7,7 +7,7 @@ const ItemListContainer = (props) =>{
             <h1 className="titule-prod">{props.name}</h1>
             <img className="img-prod" src="https://i2.wp.com/www.zarpado.com/wp-content/uploads/2020/01/01_Packshot_Air_Jordan_1_LOW_OG_Dior_93507-e1579871627241.png?fit=1000%2C667&resize=350%2C200" alt="zapatilla-nike" />
             <p className="price-prod">Precio: $13000</p>
-            <ItemCount/>
+            <ItemCount initial="1" stock="5"/>
         </div>
     )
 }
