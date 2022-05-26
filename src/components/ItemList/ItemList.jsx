8c94871567f7c5c717d8a16107465655eb7efdf9
Nahuel <coronel98nahuel@gmail.com>
2022-05-25 22:16:31 -0300

    version1.0

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Itemcard from '../ItemCard/ItemCard'
+import "./ItemList.css"
+
 
 const ItemList = ({products}) => {
   console.log(products)
   return (
-      <div>
+      <div className="box-itemlist">
         {products.map(products => <Itemcard key={products.id} {...products} />)}
       </div>
   )
