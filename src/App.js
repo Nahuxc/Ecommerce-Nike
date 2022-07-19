d4f28c8a942b8caa58dac24fc99cf729144b8066
Nahuel <coronel98nahuel@gmail.com>
2022-07-19 17:44:41 -0300

    version1.0

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import Formulario from './components/Formulario/Formulario';
 import Cart from './components/Cart/Cart';
+import Footer from './components/Footer/Footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CartContextProvider } from './components/Context/CartContext';
 import { NotificationProvider } from './Notification/notification';
@@ -26,6 +27,7 @@ const App = () => {
                   <Route path='/usuario' element={<Formulario/>}/>
                   <Route path='*' element={<h1 className='advert'>PAGE NOT FOUND 404</h1>}/>
                 </Routes>
+                <Footer/>
               </BrowserRouter>
             </NotificationProvider>
           </CartContextProvider>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,9 @@
-import { useContext } from "react";
 import { Link } from "react-router-dom";
-import CartContext from "../Context/CartContext";
 import CartWidget from "../CartWidget/CartWidget";
 import "./NavBar.css"
 
 
 const NavBar = () => {
-
-    const {getQuantity} = useContext(CartContext)
-
-    const Quantity = getQuantity()
 
     return (
         <nav className="nav">
@@ -22,7 +16,7 @@ const NavBar = () => {
                 <li className="nav-menu_li"><Link className="link-a" to="/Jordan">Nike Jordan</Link></li>
                 <li className="nav-menu_li"><Link className="link-a" to="/Roshe">Nike Roshe</Link></li>
             </ul>
-            {Quantity > 0 && <CartWidget />}
+           <CartWidget />
         </nav>
     );
 }
